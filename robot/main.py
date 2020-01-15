c0da195b20685e401e030e4f20b34aef76992f5d
Wahid Bawa <wahidbawa10@gmail.com>
2020-01-15 17:03:45 -0500

    before host

--- robot/main.py
+++ robot/main.py
@@ -25,10 +25,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # print(message.author.name, message.author.discriminator, message.author.id)
-    # print(message.channel.id)
-    # print(message.author.roles)
-    # print(message.author.id)
+    if "nuub" in [y.name.lower() for y in message.author.roles]:
+        await message.guild.get_member(message.author.id).edit(nick=str(message.clean_content))
+        role = discord.utils.get((message.author).guild.roles, name="Nuub")
+        await (message.author).remove_roles(role)
+        await message.channel.purge(limit=100)
 
     await client.process_commands(message)
 
@@ -36,6 +37,11 @@ async def on_message(message):
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Latency: {round(client.latency * 1000)}ms")
+
+
+@client.command()
+async def ziad(ctx):
+    await ctx.send("```kobti```")
 
 
 @client.command(aliases=['8ball'])
@@ -64,10 +70,10 @@ async def _8ball(ctx, *, question):
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
-@client.command()
+@client.command(aliases=['snap'])
+@commands.has_role("ADMIN")
 async def clear(ctx, amount=2):
-    if "admin" in [y.name.lower() for y in ctx.message.author.roles]:
-        await ctx.channel.purge(limit=amount)
+    await ctx.channel.purge(limit=amount)
 
 
 @client.event
@@ -75,9 +81,13 @@ async def on_member_join(member):
     print(f'{member} has joined the server.')
     # await member.send("Welcome, Please change your nickname to your real name!!!!")
     channel = client.get_channel(getChannelKey("WELCOME_CHANNEL"))
-    await channel.send(
-        'Welcome to the UWindsor Robotics & Tech Discord ' + (
-                '<@' + str(member.id) + '>') + '!!! Please change your nickname to your real name!!')
+    await channel.send('Welcome to the UWindsor Robotics & Tech Discord ' + ('<@' + str(member.id) + '>') + '!!!')
+    await channel.send("Please enter your **REAL NAME** (FIRST LAST)!!");
+
+    role = discord.utils.get(member.guild.roles, name="Nuub")
+    await member.add_roles(role)
+
+    # print()
 
 
 @client.event
