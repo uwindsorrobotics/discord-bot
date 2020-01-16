d4f8f707c6faee14d0c61bf8c47599da1196bdfc
Wahid Bawa <wahidbawa10@gmail.com>
2020-01-16 10:25:20 -0500

    added silencing feature

--- robot/main.py
+++ robot/main.py
@@ -5,14 +5,24 @@ import os
 from dotenv import load_dotenv
 import random
 
+from uwu import *
+
+import time
+
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 client = commands.Bot(command_prefix='!')
 
+silenced_dict = dict()
+
 
 def getChannelKey(channel_name):
     return int(os.getenv(channel_name))
+
+
+def getCurrTime():
+    return time.clock_gettime(time.CLOCK_REALTIME)
 
 
 @client.event
@@ -25,13 +35,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if "silenced" in [y.name.lower() for y in message.author.roles]:
+        if ((getCurrTime() - silenced_dict[message.author.nick][1]) / 60 >= silenced_dict[message.author.nick][0]):
+            role = discord.utils.get(message.author.guild.roles, name="Silenced")
+            await (message.author).remove_roles(role)
+        else:
+            await message.channel.purge(limit=1)
+
     if "nuub" in [y.name.lower() for y in message.author.roles]:
         await message.guild.get_member(message.author.id).edit(nick=str(message.clean_content))
         role = discord.utils.get((message.author).guild.roles, name="Nuub")
         await (message.author).remove_roles(role)
         await message.channel.purge(limit=100)
 
-    await client.process_commands(message)
+    if "nuub" not in [y.name.lower() for y in message.author.roles] and "silenced" not in [y.name.lower() for y in
+                                                                                           message.author.roles]:
+        await client.process_commands(message)
 
 
 @client.command()
@@ -79,7 +98,6 @@ async def clear(ctx, amount=2):
 @client.event
 async def on_member_join(member):
     print(f'{member} has joined the server.')
-    # await member.send("Welcome, Please change your nickname to your real name!!!!")
     channel = client.get_channel(getChannelKey("WELCOME_CHANNEL"))
     await channel.send('Welcome to the UWindsor Robotics & Tech Discord ' + ('<@' + str(member.id) + '>') + '!!!')
     await channel.send("Please enter your **REAL NAME** (FIRST LAST)!!");
@@ -87,12 +105,30 @@ async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, name="Nuub")
     await member.add_roles(role)
 
-    # print()
-
 
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left the server.')
+
+
+@client.command()
+async def uwu(ctx, *, message):
+    uwu = receive(message)
+    await ctx.send(uwu)
+
+
+@client.command()
+async def silence(ctx, user: discord.Member, waitTime='5m'):
+    role = discord.utils.get(user.guild.roles, name="Silenced")
+    await user.add_roles(role)
+    currTime = time.clock_gettime(time.CLOCK_REALTIME)
+
+    silenced_dict[user.display_name] = (int(waitTime.split("m")[0]), currTime)
+
+
+@client.command()
+async def out(ctx):
+    await ctx.send(silenced_dict)
 
 
 client.run(token)
